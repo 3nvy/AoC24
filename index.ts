2885c6281bd12b89f269645258241ffe4919aa3f
3nvy <renatompestana@gmail.com>
2024-12-03 21:40:16 +0000

    Add ability to select day

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import { select } from "@inquirer/prompts";
+import { readdirSync } from "fs";
+import { $ } from "zx";
+
+const dirs = readdirSync("./");
+const dayDirs = dirs
+  .filter((name) => name.startsWith("day"))
+  .sort((a, b) => +a.slice(3) - +b.slice(3));
+
+const answer = await select({
+  message: "Select a day",
+  choices: dayDirs.map((dir) => ({
+    name: dir,
+    value: dir,
+  })),
+});
+
+const log = await $`bun ./${answer}/index.ts`;
+
+console.log(log.stdout);
